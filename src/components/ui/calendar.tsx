ae221aef06212338c4d3ba98297425811f6b2664
Baswaraj T <baswarajt@gmail.com>
2025-09-03 20:41:02 +0000

    Days row - Change the colour to dark blue

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -34,7 +34,7 @@ function Calendar({
         table: "w-full border-collapse space-y-1",
         head_row: "flex w-full justify-around",
         head_cell:
-          "text-primary font-bold rounded-md w-16 text-center text-[0.8rem]",
+          "text-foreground font-bold rounded-md w-16 text-center text-[0.8rem]",
         row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
