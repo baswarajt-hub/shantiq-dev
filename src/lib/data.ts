e32891b2f69bf865dc3e2cd55de1244c524e98f9
Baswaraj T <baswarajt@gmail.com>
2025-09-06 19:53:28 +0000

    by default keep even sundays open for testing purpose

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -103,8 +103,8 @@ let doctorSchedule: DoctorSchedule = {
       evening: { start: '18:30', end: '21:30', isOpen: true },
     },
     Sunday: {
-      morning: { start: '', end: '', isOpen: false },
-      evening: { start: '', end: '', isOpen: false },
+      morning: { start: '10:30', end: '13:00', isOpen: true },
+      evening: { start: '18:30', end: '21:30', isOpen: true },
     },
   },
   specialClosures: [],
