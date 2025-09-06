96ac21f029ac7837780f35c8c91a2ec53b88df8d
Baswaraj T <baswarajt@gmail.com>
2025-09-06 09:16:33 +0000

    Change default clinic hours as Morning session 10.30 AM to 1.00 PM and e

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -78,28 +78,28 @@ let doctorSchedule: DoctorSchedule = {
   slotDuration: 15,
   days: {
     Monday: {
-      morning: { start: '09:00', end: '13:00', isOpen: true },
-      evening: { start: '16:00', end: '19:00', isOpen: true },
+      morning: { start: '10:30', end: '13:00', isOpen: true },
+      evening: { start: '18:30', end: '21:30', isOpen: true },
     },
     Tuesday: {
-      morning: { start: '09:00', end: '13:00', isOpen: true },
-      evening: { start: '16:00', end: '19:00', isOpen: true },
+      morning: { start: '10:30', end: '13:00', isOpen: true },
+      evening: { start: '18:30', end: '21:30', isOpen: true },
     },
     Wednesday: {
-      morning: { start: '09:00', end: '13:00', isOpen: true },
-      evening: { start: '16:00', end: '19:00', isOpen: true },
+      morning: { start: '10:30', end: '13:00', isOpen: true },
+      evening: { start: '18:30', end: '21:30', isOpen: true },
     },
     Thursday: {
-      morning: { start: '09:00', end: '13:00', isOpen: true },
-      evening: { start: '16:00', end: '19:00', isOpen: true },
+      morning: { start: '10:30', end: '13:00', isOpen: true },
+      evening: { start: '18:30', end: '21:30', isOpen: true },
     },
     Friday: {
-      morning: { start: '09:00', end: '13:00', isOpen: true },
-      evening: { start: '16:00', end: '19:00', isOpen: true },
+      morning: { start: '10:30', end: '13:00', isOpen: true },
+      evening: { start: '18:30', end: '21:30', isOpen: true },
     },
     Saturday: {
-      morning: { start: '10:00', end: '14:00', isOpen: true },
-      evening: { start: '16:00', end: '19:00', isOpen: true },
+      morning: { start: '10:30', end: '13:00', isOpen: true },
+      evening: { start: '18:30', end: '21:30', isOpen: true },
     },
     Sunday: {
       morning: { start: '', end: '', isOpen: false },
